db8664d68fb8c9d4ff2aa53e1465977a1f7a069c
henny_viradiya <henny.viradiya20@gmail.com>
2026-01-01 15:20:58 +0530

    brand contoller changes

--- controllers/brand.controller.js
+++ controllers/brand.controller.js
@@ -1,17 +1,28 @@
-// const DB = require('./../controllers')
+const DB = require('./../controllers')
 
-// const AddBrand = new Schema({
-//     name: { 
-//         type: String,
-//         required: true, 
-//         unique: true, 
-//         trim: true, 
-//         lowercase: true 
-//     },
-//     country: {
-//         type: String,
-//         default: 'Unknown' 
-//     },
-//     logo: String
 
-// })
+const AddBrand =  async function(req,res){
+try{
+        const {name , country}= req.body
+        if(!name) return res.json({message : 'please enter a brand name'})
+        
+        const new_brand = await DB.BRAND.create({name,country})
+        if(!new_brand) return res.json({message : 'brand is not added'})
+
+        return res.json({
+            status : 200,
+            success : true,
+            message : 'brand added successfully'
+        })
+        
+} catch (error) {
+    console.log(error);
+    res.send('something went wrong')
+    
+}
+
+}
+
+module.exports = {
+    AddBrand
+}
